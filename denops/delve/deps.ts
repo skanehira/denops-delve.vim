2b0fe5d0b4dd0b05a6f99de9a11654dfd251699b
skanehira <sho19921005@gmail.com>
2021-10-31 08:03:06 +0900

    init

---
+++ denops/delve/deps.ts
@@ -0,0 +1,4 @@
+export * from "https://deno.land/x/denops_std@v2.1.3/mod.ts";
+export * from "https://deno.land/x/denops_std@v2.1.3/variable/mod.ts";
+export * from "https://deno.land/x/unknownutil@v1.1.4/mod.ts";
+export * from "https://deno.land/std@0.113.0/testing/asserts.ts";
